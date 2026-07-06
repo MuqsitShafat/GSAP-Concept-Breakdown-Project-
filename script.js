dc73aa3db1fc42c7c1ccf70b038fcc41e7d23e08
Muqsit Shafat Hussain <hussainmuqsit65@gmail.com>
2026-07-06 15:22:03 +0500

    gsap.to, gsap.from,timelnes, custom navbar is also built with timelines synchronous animation

--- script.js
+++ script.js
@@ -28,24 +28,59 @@ gsap.from("h3",{
     // if u give stagger : -1 then it makes element to animate in reverse order 
 
 })
+// This below tells u how timelines works synchronously where we adjust delays manually 
+// gsap.to("#box2",{
+//     x: 1000,
+//     duration: 2,
+//      delay: 1,
+//      backgroundColor: "blue",
+//      scale : 2,
+// })
 
-gsap.to("#box2",{
+// gsap.to("#box3",{
+//     x: 1000,
+//     duration: 2,
+//     borderRadius : "50%",
+//      delay: 3,
+// })
+// gsap.to("#box4",{
+//     x: 1000,
+//     backgroundColor: "red",
+//     duration: 2,
+//      delay: 5,
+// })
+//  Now we will see how timelines works synchronously where we don't have to adjust delays manually
+const tl = gsap.timeline({defaults:{duration: 2}}); // we can set default values for all the animations in timeline
+tl.to("#box2",{
     x: 1000,
-    duration: 2,
-     delay: 1,
-     backgroundColor: "blue",
-     scale : 2,
+    backgroundColor: "blue",
+    scale : 2,
 })
-
-gsap.to("#box3",{
+tl.to("#box3",{
     x: 1000,
-    duration: 2,
     borderRadius : "50%",
-     delay: 3,
 })
-gsap.to("#box4",{
+tl.to("#box4",{
     x: 1000,
     backgroundColor: "red",
-    duration: 2,
-     delay: 5,
+})
+
+// Timeline animation for the navbar created 
+const navTl = gsap.timeline();
+navTl.from("#logo",{
+    y:-30,
+    duration: 0.7,
+    delay: 0.2,
+    opacity: 0,
+})  
+navTl.from("h4",{
+    y:-30,
+    duration: 1,
+    opacity: 0,
+    stagger : 0.3   
+})  
+navTl.from("#name",{
+    scale: 0.5,
+    duration: 1,
+    opacity: 0,
 })
